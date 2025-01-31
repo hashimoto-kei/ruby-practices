6676c356febe88bfc33b114dfb298c5597e611ab
hashimoto-kei <hashimoto.keigo88@gmail.com>
2025-01-31 22:37:51 +0900

    Add test

--- 04.ls/ls_test.rb
+++ 04.ls/ls_test.rb
@@ -4,5 +4,8 @@ require_relative './ls'
 class LsTest < Minitest::Test
   def test_ls
     assert_equal `ls -l`, `./ls.rb -l`
+    assert_equal `ls -al`, `./ls.rb -al`
+    assert_equal `ls -rl`, `./ls.rb -rl`
+    assert_equal `ls -arl`, `./ls.rb -arl`
   end
 end
