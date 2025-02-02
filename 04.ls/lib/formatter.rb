d8dff42716228662d0bb92ebe28a8e969f02d450
hashimoto-kei <hashimoto.keigo88@gmail.com>
2025-02-03 00:14:43 +0900

    Refactoring

--- 04.ls/lib/formatter.rb
+++ 04.ls/lib/formatter.rb
@@ -5,19 +5,15 @@ require_relative 'entry'
 
 class Formatter
   MAX_COLUMNS = 3
-  BLANK = Entry.new
+  BLANK = ''
 
   def self.format(entries)
     max_file_name_length = entries.map(&:file_name).map(&:length).max
-    rows = ((entries.size - 1) / MAX_COLUMNS) + 1
-    matrix = entries.each_slice(rows).to_a
+    file_names = entries.map(&:file_name).map { |file_name| file_name.ljust(max_file_name_length) }
+    rows = ((file_names.size - 1) / MAX_COLUMNS) + 1
+    matrix = file_names.each_slice(rows).to_a
     matrix[-1] << BLANK until matrix[-1].size % rows == 0
-    matrix = matrix.map do |column|
-      column.map(&:file_name).map{ |file_name| file_name.ljust(max_file_name_length) }
-    end
-    rows = matrix.transpose.map do |row|
-      row.join("\t")
-    end
+    rows = matrix.transpose.map { |row| row.join("\t") }
     rows.join("\n")
   end
 end
