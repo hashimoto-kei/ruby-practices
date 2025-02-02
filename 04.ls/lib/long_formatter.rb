b328c834528ee155e0859b3d264729fe3823a016
hashimoto-kei <hashimoto.keigo88@gmail.com>
2025-02-03 00:02:37 +0900

    Refactoring

--- 04.ls/lib/long_formatter.rb
+++ 04.ls/lib/long_formatter.rb
@@ -5,10 +5,10 @@ require_relative 'entry'
 
 class LongFormatter
   def self.format(entries)
-    digits = entries.map(&:nlink).max.digits.size
-    size = entries.map(&:size).max.digits.size
+    max_nlink_digits = entries.map(&:nlink).max.digits.size
+    max_size_digits = entries.map(&:size).max.digits.size
     rows = entries.map do |entry|
-      "#{entry.file_type}#{entry.permissions}  #{entry.nlink.to_s.rjust(digits)} #{entry.user_name}  #{entry.group_name}  #{entry.size.to_s.rjust(size)} #{entry.timestamp} #{entry.file_name}#{entry.symbolic_link}"
+      "#{entry.file_type}#{entry.permissions}  #{entry.nlink.to_s.rjust(max_nlink_digits)} #{entry.user_name}  #{entry.group_name}  #{entry.size.to_s.rjust(max_size_digits)} #{entry.timestamp} #{entry.file_name}#{entry.symbolic_link}"
     end
     total = entries.map(&:blocks).sum
     rows = ["total #{total}", *rows]
