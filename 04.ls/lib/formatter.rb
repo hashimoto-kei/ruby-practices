8ede25b2de8ad4bee9400111d9c7cd1e2911845b
hashimoto-kei <hashimoto.keigo88@gmail.com>
2025-02-11 14:11:26 +0900

    Change variable name (number => size)

--- 04.ls/lib/formatter.rb
+++ 04.ls/lib/formatter.rb
@@ -8,8 +8,8 @@ class Formatter
     file_names = entries.map(&:file_name)
     max_file_name_length = file_names.map(&:length).max
     file_names << BLANK until (file_names.size % MAX_COLUMNS).zero?
-    row_number = file_names.size / MAX_COLUMNS
-    rows = file_names.each_slice(row_number).to_a.transpose.map do |row|
+    row_size = file_names.size / MAX_COLUMNS
+    rows = file_names.each_slice(row_size).to_a.transpose.map do |row|
       row = row.compact.map { |file_name| file_name.ljust(max_file_name_length) }
       row.join("\t")
     end
