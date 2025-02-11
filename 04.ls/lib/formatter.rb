6b14cb3df1ba183fb04b3cb25756738c24b97da8
hashimoto-kei <hashimoto.keigo88@gmail.com>
2025-02-11 14:07:49 +0900

    Use compact instead of filter(&:itself)

--- 04.ls/lib/formatter.rb
+++ 04.ls/lib/formatter.rb
@@ -10,7 +10,7 @@ class Formatter
     file_names << BLANK until (file_names.size % MAX_COLUMNS).zero?
     row_number = file_names.size / MAX_COLUMNS
     rows = file_names.each_slice(row_number).to_a.transpose.map do |row|
-      row = row.filter(&:itself).map { |file_name| file_name.ljust(max_file_name_length) }
+      row = row.compact.map { |file_name| file_name.ljust(max_file_name_length) }
       row.join("\t")
     end
     rows.join("\n")
