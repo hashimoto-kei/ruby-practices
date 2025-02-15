fb73833f588fad47f830a7facf81d879fe65e92d
hashimoto-kei <hashimoto.keigo88@gmail.com>
2025-02-15 17:33:48 +0900

    Add -a option

--- 04.ls/lib/ls_command.rb
+++ 04.ls/lib/ls_command.rb
@@ -4,12 +4,20 @@ require_relative 'ls_file'
 require_relative 'formatter'
 
 class LsCommand
-  def initialize(path)
+  def initialize(option, path)
+    @option = option
     @path = File.expand_path(path || '.')
   end
 
   def execute
-    ls_files = Dir.entries(@path).sort.map { |entry| LsFile.new(entry) }.reject(&:hidden?)
-    puts Formatter.format(ls_files)
+    ls_files = Dir.entries(@path).sort.map { |entry| LsFile.new(entry) }
+    filtered_ls_files = filter_ls_files(ls_files)
+    puts Formatter.format(filtered_ls_files)
+  end
+
+  private
+
+  def filter_ls_files(ls_files)
+    @option[:a] ? ls_files : ls_files.reject(&:hidden?)
   end
 end
--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,8 +1,10 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'optparse'
 require_relative 'lib/ls_command'
 
+option = ARGV.getopts('a').transform_keys(&:to_sym)
 path = ARGV[0]
 
-LsCommand.new(path).execute
+LsCommand.new(option, path).execute
