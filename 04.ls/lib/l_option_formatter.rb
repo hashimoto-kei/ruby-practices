c20dcbf49d9565598a88e8f58273b8230da40dce
hashimoto-kei <hashimoto.keigo88@gmail.com>
2025-02-27 00:52:04 +0900

    Use strftime method of Time Object

--- 04.ls/lib/l_option_formatter.rb
+++ 04.ls/lib/l_option_formatter.rb
@@ -20,13 +20,12 @@ class LOptionFormatter
       max_widths = calculate_max_widths(ls_files, now)
       ls_files.map do |ls_file|
         formatted_file_name = format_file_name(ls_file.name, ls_file.link_name)
-        formatted_timestamp = format_timestamp(ls_file.timestamp)
         "#{ls_file.type}#{ls_file.mode}" \
         "  #{ls_file.hard_links.to_s.rjust(max_widths[:hard_links])}" \
         " #{ls_file.owner_name.ljust(max_widths[:owner_name])}" \
         "  #{ls_file.group_name.ljust(max_widths[:group_name])}" \
         "  #{ls_file.size.to_s.rjust(max_widths[:size])}" \
-        " #{formatted_timestamp}" \
+        " #{ls_file.timestamp.strftime('%_m %_d %H:%M')}" \
         " #{formatted_file_name}"
       end
     end
@@ -43,16 +42,6 @@ class LOptionFormatter
         widths.each_key { |key| max_widths[key] = widths[key] if max_widths[key] < widths[key] }
       end
       max_widths
-    end
-
-    def format_timestamp(timestamp)
-      timestamp.to_s.match(/(\d{4})-(\d{2})-(\d{2}) (\d{2}:\d{2}).*/) do
-        year = Regexp.last_match(1)
-        month = Regexp.last_match(2)
-        day = Regexp.last_match(3)
-        time = Regexp.last_match(4)
-        "#{month.to_i.to_s.rjust(2)} #{day.to_i.to_s.rjust(2)} #{time}"
-      end
     end
 
     def format_file_name(file_name, link_name)
