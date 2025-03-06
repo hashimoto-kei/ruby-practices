02378e3e2c0b9723621d6d182a5a0e15862c87b8
hashimoto-kei <hashimoto.keigo88@gmail.com>
2025-03-07 01:33:45 +0900

    Add tests

--- 05.wc/wc_test.rb
+++ 05.wc/wc_test.rb
@@ -3,7 +3,7 @@
 require 'minitest/autorun'
 
 class WcTest < Minitest::Test
-  def test_wc
+  def test_wc_one_file
     assert_equal `wc fileA`, `./wc.rb fileA`
     assert_equal `wc -l fileA`, `./wc.rb -l fileA`
     assert_equal `wc -w fileA`, `./wc.rb -w fileA`
@@ -12,6 +12,9 @@ class WcTest < Minitest::Test
     assert_equal `wc -wc fileA`, `./wc.rb -wc fileA`
     assert_equal `wc -cl fileA`, `./wc.rb -cl fileA`
     assert_equal `wc -lwc fileA`, `./wc.rb -lwc fileA`
+  end
+
+  def test_wc_two_files
     assert_equal `wc fileA fileB`, `./wc.rb fileA fileB`
     assert_equal `wc -l fileA fileB`, `./wc.rb -l fileA fileB`
     assert_equal `wc -w fileA fileB`, `./wc.rb -w fileA fileB`
@@ -20,5 +23,16 @@ class WcTest < Minitest::Test
     assert_equal `wc -wc fileA fileB`, `./wc.rb -wc fileA fileB`
     assert_equal `wc -cl fileA fileB`, `./wc.rb -cl fileA fileB`
     assert_equal `wc -lwc fileA fileB`, `./wc.rb -lwc fileA fileB`
+  end
+
+  def test_wc_stdin
+    assert_equal `ls -l | wc `, `ls -l | ./wc.rb`
+    assert_equal `ls -l | wc -l`, `ls -l | ./wc.rb -l`
+    assert_equal `ls -l | wc -w`, `ls -l | ./wc.rb -w`
+    assert_equal `ls -l | wc -c`, `ls -l | ./wc.rb -c`
+    assert_equal `ls -l | wc -lw`, `ls -l | ./wc.rb -lw`
+    assert_equal `ls -l | wc -wc`, `ls -l | ./wc.rb -wc`
+    assert_equal `ls -l | wc -cl`, `ls -l | ./wc.rb -cl`
+    assert_equal `ls -l | wc -lwc`, `ls -l | ./wc.rb -lwc`
   end
 end
